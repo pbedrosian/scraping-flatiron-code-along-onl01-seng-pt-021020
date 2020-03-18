0e2a1bad41fbf7513d0242608b1a843c3fb5f69f
Peter Bedrosian <peter_bedrosian@yahoo.com>
2020-03-18 11:58:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -19,16 +19,9 @@ class Scraper
       binding.pry
     end
 
-  # def print_courses
-  #   self.make_courses
-  #   Course.all.each do |course|
-  #     if course.title && course.title != ""
-  #       puts "Title: #{course.title}"
-  #       puts "  Schedule: #{course.schedule}"
-  #       puts "  Description: #{course.description}"
-  #     end
-  #   end
-  # end
+    def get_courses
+      self.get_page.css(".post")
+    end
 
 end
 
